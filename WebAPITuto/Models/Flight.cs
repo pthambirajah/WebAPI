d529550e2ba054e81a8b52101cd976b772599cc3
Piranavan Thambirajah <piranavanpth@gmail.com>
2020-05-28 11:13:45 +0200

    Ajoutez des fichiers projet.

---
+++ WebAPITuto/Models/Flight.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPITuto.Models
+{
+    public class Flight
+    {
+        [Key]
+        public int FlightNo { get; set; }
+
+        [StringLength(50), MinLength(3)]
+        public string Departure { get; set; }
+
+        [StringLength(50), MinLength(3)]
+        public string Destination { get; set; }
+
+        public DateTime Date { get; set; }
+
+        [Required]
+        public short? Seats { get; set; }
+    }
+}
---
+++ WebAPITuto/Models/Passenger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPITuto.Models
+{
+    public class Passenger
+    {
+        [Key]
+        public int PersonID { get; set; }
+
+        public string Surname { get; set; }
+
+        public string GivenName { get; set; }
+    }
+}
